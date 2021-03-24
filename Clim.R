629a8c079b723c92bb077ca83f7cb62beb69e1a1
Jared Zystro <jared@seedalliance.org>
2021-03-23 17:36:01 -0700

    Added season

--- Clim.R
+++ Clim.R
@@ -12,21 +12,34 @@ names(r_colors) <- colors()
 
 ui <- fluidPage(
   div(id="intro", HTML("<h2>Future Climate Analogs Map</h2><br><p>
-  This site lets you view a map showing locations that are currently
-  similar to what your location in the future.<br>
+  <h4>This site lets you view a map showing locations that are currently
+  similar to what your location in the future.
   To use, select how far into future you want to look, and select whether
-  you want to compare based on temperature, precipitation, or both.<br>
-  Note, the map will take a while to load. Please be patient.</p>")),
-  radioButtons("year","Future Year",c("2030","2050","2070")),
-  radioButtons("compare","Look for Similarities based on",c("Temperature","Precipitation","Both")),
+  you want to compare based on temperature, precipitation, or both.</h4><br></p>")),
+  div(style="display:inline-block",selectInput("year", "Future Year", choices=c("2030","2050","2070"))),
+  div(style="display:inline-block",selectInput("startgrow", "Start of the growing season", choices=c("Jan" = 1,"Feb" = 2,"Mar" = 3, 
+                                                                      "Apr" = 4, "May" = 5, "Jun" = 6, 
+                                                                      "Jul" = 7, "Aug" = 8, "Sep" = 9, 
+                                                                      "Oct" = 10, "Nov" = 11, "Dec" = 12), selected = 1)),
+  div(HTML("<br>")),
+    div(style="display:inline-block",selectInput("compare","Look for Similarities based on",c("Temperature","Precipitation","Both"))),
+    div(style="display:inline-block",selectInput("endgrow","End of the growing season",choices=c("Jan" = 1,"Feb" = 2,"Mar" = 3, 
+                                                                "Apr" = 4, "May" = 5, "Jun" = 6, 
+                                                                "Jul" = 7, "Aug" = 8, "Sep" = 9, 
+                                                                "Oct" = 10, "Nov" = 11, "Dec" = 12), selected = 12)),
+  
   div(id="sel_map", HTML("<p>Zoom and click on the map to select the location you want to look for analogs for</p>
-                         <p style='color:red;'><b>NOTE:</b> Even if you use the address bar to navigate to a location, you must click on the map to set your location</p>")),
-  leafletOutput("mymap"),
-  submitButton("Create Map"),
-  textOutput("lat"),
-  div(id="disp_map", HTML("<p>A large map will load below with the locations that have current climates similar to your future climate. 
-                          Green and yellow shading are locations that are more similar<b>")),
-  plotOutput("AnalogPlot",width=2000,height=2000)
+                         <p style='color:red;'><b>NOTE:</b> Even if you use the address bar to navigate, you must click on the map to set your location</p>")),
+  leafletOutput("mymap", width = "100%", height = "300px"),
+  div(HTML("<br>")),
+  actionButton("submit_button","Create Analog Map"),
+ # textOutput("lat"),
+  div(id="disp_map", HTML("<br><p><b>A map will load below with the locations that have current climates similar to your future climate. 
+                          <br>Green and yellow shading are locations that are more similar.</b></p><br>")),
+  plotOutput("AnalogPlot",width = "100%", height = "800px"),
+  div(HTML("Code based on analogues R package from Julian Ramirez of CIAT, 
+            using current data from worldclim, future data from CMIP5, 
+           using the 8.5 relative concentration BCC-CSM1.1 climate model"))
 )
 
 server <- function(input, output, session) {
@@ -48,64 +61,74 @@ server <- function(input, output, session) {
       addMarkers(lng = click$lng, lat = click$lat)
   })
   
-  output$lat <- renderText({
-    
-    print(c("Latitude: ",input$mymap_click$lat,", Longitude:",input$mymap_click$lng))
-    
-  })
+  # output$lat <- renderText({
+  #   
+  #   print(c("Latitude: ",input$mymap_click$lat,", Longitude:",input$mymap_click$lng))
+  #   
+  # })
 
  output$AnalogPlot <- renderPlot({
    
-   # wait for map click  
-   validate(need(input$mymap_click, 'Map will load after location, date, and comparison type are chosen'))
-#   validate(need(input$mymap_click, 'Please click on a point on the map'))
+   input$submit_button
+   
+   # Isolate variables
+   compare <- isolate(input$compare)
+   year <- isolate(input$year)
+   lat <- isolate(input$mymap_click$lat)
+   lng <- isolate(input$mymap_click$lng)
+   startgrow <- isolate(input$startgrow)
+   endgrow <- isolate(input$endgrow)
+
+  # wait for map click  
+   validate(need(input$submit_button, 'Map will load after location, date, and comparison type are chosen'))
+   validate(need(lat, 'Please click on a point on the map and then click the Create Analog Map button'))
    withProgress(message = "Creating analog map: ", value = 0, {
      
     # get precipitation data
     incProgress(0.333, detail = "Loading data")
-    if (input$compare == "Precipitation" ||  input$compare == "Both") {
+    if (compare == "Precipitation" ||  compare == "Both") {
       cur_prec <- raster::getData("worldclim", res=10, var="prec", path=".")
-      fut_prec <- getCMIP5(var="prec", rcp=8.5, model=1, year=as.numeric(input$year), res=10, path='.')
+      fut_prec <- getCMIP5(var="prec", rcp=8.5, model=1, year=as.numeric(year), res=10, path='.')
    }
    
-    if (input$compare == "Temperature" ||  input$compare == "Both") {
+    if (compare == "Temperature" ||  compare == "Both") {
     cur_temp <- raster::getData("worldclim", res=10, var="tmean", path=".")
-     fut_temp <- getCMIP5(var="tmean", rcp=8.5, model=1, year=as.numeric(input$year), res=10, path='.')
+     fut_temp <- getCMIP5(var="tmean", rcp=8.5, model=1, year=as.numeric(year), res=10, path='.')
    }
   
   # match up coordinates
   
-     if (input$compare == "Temperature") {
+     if (compare == "Temperature") {
        crs(fut_temp) <- crs(cur_temp) 
        }
      
-     if (input$compare == "Precipitation") {
+     if (compare == "Precipitation") {
        crs(fut_prec) <- crs(cur_prec) 
        }
      
-     if (input$compare == "Both") {
+     if (compare == "Both") {
        crs(fut_temp) <- crs(fut_prec) <- cur_prec
        }
   
   # create parameter object
     incProgress(0.333, detail = "Creating model")
-    if (input$compare == "Precipitation") {
-      params <- createParameters(x=input$mymap_click$lng, y=input$mymap_click$lat, vars=c("prec"),weights=c(1),
-                                ndivisions=c(12,12),growing.season=c(1,12),rotation="prec",threshold=1,
+    if (compare == "Precipitation") {
+      params <- createParameters(x=lng, y=lat, vars=c("prec"),weights=c(1),
+                                ndivisions=c(12,12),growing.season=c(startgrow,endgrow),rotation="prec",threshold=1,
                                  env.data.ref=list(cur_prec), env.data.targ=list(fut_prec),
                                  outfile="~/.",fname=NA,writefile=FALSE)
     }
   
-    if (input$compare == "Temperature") {
-      params <- createParameters(x=input$mymap_click$lng, y=input$mymap_click$lat, vars=c("tmean"),weights=c(1),
-                                 ndivisions=c(12,12),growing.season=c(1,12),rotation="tmean",threshold=1,
+    if (compare == "Temperature") {
+      params <- createParameters(x=lng, y=lat, vars=c("tmean"),weights=c(1),
+                                 ndivisions=c(12,12),growing.season=c(startgrow,endgrow),rotation="tmean",threshold=1,
                                  env.data.ref=list(cur_temp), env.data.targ=list(fut_temp),
                                  outfile="~/.",fname=NA,writefile=FALSE)
     }
     
-    if (input$compare == "Both") {
-      params <- createParameters(x=input$mymap_click$lng, y=input$mymap_click$lat, vars=c("prec","tmean"),weights=c(0.5,0.5),
-                                 ndivisions=c(12,12),growing.season=c(1,12),rotation="tmean",threshold=1,
+    if (compare == "Both") {
+      params <- createParameters(x=lng, y=lat, vars=c("prec","tmean"),weights=c(0.5,0.5),
+                                 ndivisions=c(12,12),growing.season=c(startgrow,endgrow),rotation="tmean",threshold=1,
                                  env.data.ref=list(cur_prec,cur_temp), env.data.targ=list(fut_prec,fut_temp),
                                  outfile="~/.",fname=NA,writefile=FALSE)
     }
@@ -117,7 +140,7 @@ server <- function(input, output, session) {
     # plot the result
     incProgress(0.333, detail = "Drawing map")
     
-    plot(sim_out, zlim=c(0,1), col = rev(terrain.colors(10)))
+    plot(sim_out, col = rev(terrain.colors(10)))
     plot(wrld_simpl, add=T)
     
    })
